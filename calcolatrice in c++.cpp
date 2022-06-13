5a6e3b520007c82b7fd9c810346b134badde3e2f
Cerby <94308301+Cerby12@users.noreply.github.com>
2022-06-13 02:24:35 +0200

    Add files via upload

---
+++ calcolatrice in c++.cpp
@@ -0,0 +1,47 @@
+
+
+#include <iostream>
+#include <string>
+
+
+
+void calculetor() {
+	int numero1;
+	int numero2;
+	std::string che_calcolo_vuoi_fare;
+	std::cout << "insert first number: ";
+
+	std::cin >> numero1;
+
+	std::cout << "insert secondary number: ";
+
+	std::cin >> numero2;
+
+	std::cout << "insert operetor: ";
+
+	std::cin >> che_calcolo_vuoi_fare;
+
+	if (che_calcolo_vuoi_fare == "+") {
+		std::cout << numero1 + numero2;
+	}
+	else if (che_calcolo_vuoi_fare == "-") {
+		std::cout << numero1 - numero2;
+	}
+	else if (che_calcolo_vuoi_fare == "*") {
+		std::cout << numero1 * numero2;
+	}
+	else if (che_calcolo_vuoi_fare == "/") {
+		std::cout << numero1 / numero2;
+	}
+
+	else
+	{
+		std::cout << "uhm operetore invalido\n impossibile fare il calcolo";
+	}
+
+}
+
+
+int main() {
+	calculetor();
+}
